012378bf49da25f31d62e0069f09d9344f4877c2
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-05-21 17:09:54 +0530

    Packag

---
+++ my_script.sh
@@ -0,0 +1,25 @@
+echo -e "\e[33m[01]  Start Installation.
+[02]  Update Script.
+[03]  Delete Script.
+[04] About Me.
+[05] Exit.\e[0m";
+echo -e "";
+echo -e "\e[31mChoose Your Options : -\e[0m";
+if [ "${userinput0:-}" = "1" ]
+then
+	echo -e "\e[33m------------------ɪɴꜱᴛᴀʟʟᴀᴛɪᴏɴ ʜᴀꜱ ʙᴇᴇɴ ꜱᴛᴀʀᴛᴇᴅ.....\e[0m";
+fi
+if [ "${userinput0:-}" = "2" ]
+then
+	echo -e "\e[35m-------ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴛɪʟʟ ꜱᴄʀɪᴘᴛ ᴜᴘᴅᴀᴛɪɴɢ--------
+\e[0m";
+fi
+if [ "${userinput0:-}" = "3" ]
+then
+	echo -e "\e[32m--------WAIT SCRIPT DELETING ARE IN PROCESS-----------\e[0m";
+fi
+if [ "${userinput0:-}" = "4" ]
+then
+	echo -e "\e[33mʜᴇʟʟᴏ ꜰʀɪᴇɴᴅꜱ ,
+          ɪ'ᴍ ᴘʀᴀʟʜᴀᴅ ɴᴀꜱᴀɴᴇ . ᴇɴᴛʜᴜꜱɪᴀꜱᴛɪᴄ ᴛᴏ ʟᴀᴛᴇꜱᴛ ᴛᴇᴄʜɴᴏʟᴏɢʏ .\e[0m";
+fi
